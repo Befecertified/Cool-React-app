5a2d3b81c40b52241b9007a18108d615f9eb7541
befecertified <deekor.baribefe@gmail.com>
2018-10-22 01:59:17 +0100

    Some components created.

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <span className={this.changeButtonClasses()}>
+          {this.formatCounterValue()}
+        </span>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  changeButtonClasses() {
+    let classes = "badge m-2 badge-";
+    return this.props.counter.value === 0
+      ? (classes += "warning")
+      : (classes += "primary");
+  }
+
+  formatCounterValue() {
+    const { value } = this.props.counter;
+
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
---
+++ srcss/index.js
@@ -0,0 +1,15 @@
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+
+// class FirstComponent extends Component {
+//     render(){
+//         return (
+//             <div>
+//                 <h1> Hello World </h1>
+//                 <p> Welcome {this.props.user} </p>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<FirstComponent user="Befe"/>, document.getElementById("root"));
