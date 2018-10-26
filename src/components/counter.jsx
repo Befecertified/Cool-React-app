6f43bc8febd9f0a25e84e7ff94224d807e902ad7
befecertified <deekor.baribefe@gmail.com>
2018-10-26 01:29:30 +0100

    Made some changes to existing components and deleted an unused folder.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <span className={this.changeButtonClasses()}>
-          {this.formatCounterValue()}
-        </span>
-        <button
-          className="btn btn-secondary btn-sm"
-          onClick={() => this.props.onIncrement(this.props.counter)}
-        >
-          Increment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
-      </div>
-    );
-  }
-
   changeButtonClasses() {
     let classes = "badge m-2 badge-";
     return this.props.counter.value === 0
@@ -34,6 +12,45 @@ class Counter extends Component {
     const { value } = this.props.counter;
 
     return value === 0 ? "Zero" : value;
+  }
+
+  disableButtonClass() {
+    let classes = "btn btn-secondary btn-sm m-2";
+    if (this.props.counter.value === 0) classes += " disabled";
+    return classes;
+  }
+
+  render() {
+    const { onIncrement, onDecrement, onDelete, counter } = this.props;
+    return (
+      <div className="row">
+        <div className="col-1">
+          <span className={this.changeButtonClasses()}>
+            {this.formatCounterValue()}
+          </span>
+        </div>
+        <div className="col">
+          <button
+            className="btn btn-secondary btn-sm"
+            onClick={() => onIncrement(counter)}
+          >
+            +
+          </button>
+          <button
+            className={this.disableButtonClass()}
+            onClick={() => onDecrement(counter)}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => onDelete(counter.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
